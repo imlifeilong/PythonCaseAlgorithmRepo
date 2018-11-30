0014bb1f1a5a32a81df7eb473c49e928d08f93cb
lifeilong <lifeilong@togeek.cn>
2018-11-30 10:01:26 +0800

    快排改进

--- 快排改进.py
+++ 快排改进.py
@@ -1,9 +1,10 @@
 import random
 
 def partaion(data, left, right):
-    less = left - 1
-    more = right
+    less = left - 1 #小区域是从列表初始位置前一位开始
+    more = right   #大区域是从列表末尾位置开始，因为末尾位置的数是分隔值，可以视为末尾位置无值
 
+    # 遍历整个待定区域  left < 待定区域 < more
     while left < more:
         if data[left] < data[right]:
             less += 1
@@ -14,17 +15,25 @@ def partaion(data, left, right):
             data[left], data[more] = data[more], data[left]
         else:
             left += 1
+    # 整个列表分隔完后，此时left已经来到大区域的第一个位置（等于区 < left 大于区域），和末尾值交换（末尾值是分隔值）
     data[more], data[right] = data[right], data[more]
-
+    # 此时返回小于区域的末尾边界 和大于区域的开始边界
     return less, more+1
 
 def sort(data, left, right):
+    # 如果列表为空或者，所需区域不存在，则返回
     if not data or left > right:
         return
+    # 随机选择一位作为分界点，将整个排序过程变成时间复杂度的期望值
     index = random.randint(left, right)
+    # 交换到末尾，节省一个变量
     data[right], data[index] = data[right], data[index]
+    
+    # 将列表的数按分隔值分开
     mid = partaion(data, left, right)
+    # 递归排序分隔值左边的列表
     sort(data, left, mid[0])
+    # 递归排序分隔值右边的列表
     sort(data, mid[1], right)
     
     return data
--- 荷兰国旗.py
+++ 荷兰国旗.py
@@ -5,32 +5,31 @@
 要求额外空间复杂度O(1)，时间复杂度O(N)
 '''
 
-def netherlands(data, num):
-    length = len(data)
-    less = -1 #小于区域
-    more = length #大于区域
-    index = 0
+def netherlands(data, left, right, num):
+    less = -1 #小于区域，从列表初始位置前一位开始
+    more = right #大于区域， 从列表末尾位置后一位开始
     # [小于区域，等于区域， 待定区域， 大于区域]
-    # 
-    while index < more:
+    
+    # 只对待定区域遍历left左边是小于，等于， more右边是大于，之间是待定区域
+    while left < more:
         # 如果当前数据小于 值，小于区域向下一位，并和当前数交换，当前位置向下一位
-        if data[index] < num:
+        if data[left] < num:
             less += 1
-            data[index], data[less] = data[less], data[index]
-            index += 1
+            data[left], data[less] = data[less], data[left]
+            left += 1
         # 如果当前数大于 值，大于区域向前一位，并和当前值交换
-        elif data[index] > num:
+        elif data[left] > num:
             more -= 1
-            data[index], data[more] = data[more], data[index]
+            data[left], data[more] = data[more], data[left]
         # 如果等于 值，直接跳下一位
         else:
-            index += 1
-    print(less+1, more-1)
+            left += 1
+    print(less, more)
     return data
 
 
 if __name__ == '__main__':
     a = [5, 3, 4, 2, 6, 1, 7, 8, 5, 9, 5, 5, 6, 5]
     b = [0,2,1,2,2,1,1,0,2,1,0]
-    print(netherlands(a, 5))
+    print(netherlands(b, 0, len(b), 1))
 
