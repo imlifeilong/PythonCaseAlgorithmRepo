93ed9bbc7e9ca126964a4c4268ce386180739c73
lifeilong <lifeilong@togeek.cn>
2019-01-25 09:44:35 +0800

    图

--- 图-图遍历.py
+++ 图-图遍历.py
@@ -1,6 +1,14 @@
 '''
-
-
+宽度优先遍历
+    1、利用队列实现
+    2、从源节点开始依次按照宽度进队列，然后弹出
+    3、每弹出一个点，把该节点所有没有进过队列的邻接点放入队列
+    4、直到队列变空
+深度优先遍历
+    1、利用栈实现
+    2、从源节点开始把节点按照深度放入栈，然后弹出
+    3、每弹出一个点，把该节点下一个没有进过栈的邻接点放入栈
+    4、直到栈变空
 '''
 from queue import Queue
 
@@ -141,6 +149,6 @@ if __name__ == '__main__':
     '''
     g = CreateGraph(m)
     gtbfs = GraphTraverse()
-    # gtbfs.bfs(g.nodes[1])
-    gtbfs.dfs(g.nodes[1])
+    gtbfs.bfs(g.nodes[1])
+    # gtbfs.dfs(g.nodes[1])
 
