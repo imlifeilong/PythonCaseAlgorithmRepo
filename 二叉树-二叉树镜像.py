32dc78d3da036137ab767f10a13188091130387e
project <13772032410@163.com>
2020-01-16 11:51:41 +0800

    Update 二叉树-二叉树镜像.py

--- 二叉树-二叉树镜像.py
+++ 二叉树-二叉树镜像.py
@@ -13,18 +13,25 @@ class Node():
         self.right = right
 
 
-class TreeMirror():
-    def image(self, tree):
-        if not tree:
-            return
-        if not tree.left and not tree.right:
-        	return
+class MirroredTree():
+	def is_mirrored(self, tree):
+		# 空树对称
+		if tree is None:
+			return True
+		return self.handler(tree.left, tree.right)
 
-        tree.left, tree.right = tree.right, tree.left
-        if tree.left:
-        	self.image(tree.left)
-        if tree.right:
-        	self.image(tree.right)
+	def handler(self, left, right):
+		# 无左右孩子页对称
+		if left is None and right is None:
+			return True
+		# 左右只要又其中一个就不对称
+		if left is None or right is None:
+			return False
+
+		if left and right:
+			# 判断值是否相等，再递归 判断左孩子的左与右孩子的右，左孩子的右与右孩子的左是否相等
+			return left.root == right.root and self.handler(left.left, right.right) and self.handler(left.right, right.left)
+
 
 def printTree(root):
     if not root:
@@ -59,7 +66,7 @@ if __name__ == '__main__':
     '''
     ct = Node('A',Node('B',Node('D'),Node('E')),Node('C',right=Node('F',Node('G'))))
     printTree(ct)
-    tt = TreeMirror()
-    tt.image(ct)
+    tt = MirroredTree()
+    tt.is_mirrored(ct)
     printTree(ct)
 
