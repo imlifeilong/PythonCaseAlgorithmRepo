04823911abbe8e628c9298164cd38bc136d56e37
imlifeilong <13772032410@163.com>
2021-03-08 22:05:59 +0800

    3-8

--- mcode/回溯/leetcode22-括号生成.py
+++ mcode/回溯/leetcode22-括号生成.py
@@ -1,4 +1,27 @@
 from typing import List
+
+'''
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+ 
+
+示例 1：
+
+输入：n = 3
+输出：["((()))","(()())","(())()","()(())","()()()"]
+示例 2：
+
+输入：n = 1
+输出：["()"]
+ 
+
+提示：
+
+1 <= n <= 8
+
+'''
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         self.result = []
@@ -11,11 +34,10 @@ class Solution:
             return
         # 左括号可以无限入
         if left < n:
-            self.dfs(left+1, right, n, s+'(')
+            self.dfs(left + 1, right, n, s + '(')
         # 右括号要比左括号少的时候，才能入
-        if right<left and right < n:
-            self.dfs(left, right+1, n, s+')')
-
+        if right < left and right < n:
+            self.dfs(left, right + 1, n, s + ')')
 
 
 if __name__ == '__main__':
--- mcode/回溯/leetcode46-全排列.py
+++ mcode/回溯/leetcode46-全排列.py
@@ -1,5 +1,24 @@
 from typing import List
 
+'''
+给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+
+示例:
+
+输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+
+'''
+
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
