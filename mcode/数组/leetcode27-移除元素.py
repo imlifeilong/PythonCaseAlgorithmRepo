dcbd85e6b5cdb0c63994e1101c4753d660d02ed2
imlifeilong <13772032410@163.com>
2021-03-09 23:24:21 +0800

    3-9

---
+++ mcode/数组/leetcode27-移除元素.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        '''双指针，快慢指针，时间复杂度O(n) 空间复杂度O(1)'''
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        print(index, nums)
+        return index
+
+
+if __name__ == '__main__':
+    nums = [0, 1, 2, 2, 3, 0, 4, 2]
+    val = 2
+    s = Solution()
+    s.removeElement(nums, val)
