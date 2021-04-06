a8dd25ff3a8c7af1acb51a3902cb8a7e084b2535
imlifeilong <13772032410@163.com>
2021-04-06 11:25:12 +0800

    4-6

--- basic/排序/排序-快排改进.py
+++ basic/排序/排序-快排改进.py
@@ -1,10 +1,11 @@
 import random
 
-def partaion(data, left, right):
-    less = left - 1 #小区域是从列表初始位置前一位开始
-    more = right   #大区域是从列表末尾位置开始，因为末尾位置的数是分隔值，可以视为末尾位置无值
 
-    # 遍历整个待定区域  left < 待定区域 < more
+def partaion(data, left, right):
+    less = left - 1  # 小区域是从列表初始位置前一位开始
+    more = right  # 大区域是从列表末尾位置开始，因为末尾位置的数是分隔值，可以视为末尾位置无值
+
+    # 遍历整个待定区域  小于区域(less指向小于区域最右)<等于区域<left(指向待定区域最左)< 待定区域 < more
     while left < more:
         # data[right] 是选择的那个比较项
         # 如果数 小于 比较项，小于区向右移动一位
@@ -22,7 +23,8 @@ def partaion(data, left, right):
     # 整个列表分隔完后，此时left已经来到大区域的第一个位置（等于区 < left 大于区域），和末尾值交换（末尾值是分隔值）
     data[more], data[right] = data[right], data[more]
     # 此时返回小于区域的末尾边界 和大于区域的开始边界
-    return less, more+1
+    return less, more + 1
+
 
 def sort(data, left, right):
     # 如果列表为空或者，所需区域不存在，则返回
@@ -32,19 +34,20 @@ def sort(data, left, right):
     index = random.randint(left, right)
     # 交换到末尾，节省一个变量
     data[right], data[index] = data[right], data[index]
-    
+
     # 将列表的数按分隔值分开
     mid = partaion(data, left, right)
     # 递归排序分隔值左边的列表
     sort(data, left, mid[0])
     # 递归排序分隔值右边的列表
     sort(data, mid[1], right)
-    
+
     return data
 
 
 if __name__ == '__main__':
     a = [5, 3, 4, 2, 6, 1, 1, 7, 8, 5, 9, 5, 5, 6, 5]
     a = [-1, -1, 0, -3, -99, 9, 11, 2, 5, 0]
-    a = [27, 13, 30, 14, 11, 11,13,13, 5,  6, 16, 18, 7, 17, 5,11,13]
-    print(sort(a, 0, len(a)-1))
+    a = [27, 13, 30, 14, 11, 11, 13, 13, 5, 6, 16, 18, 7, 17, 5, 11, 13]
+    a = [2,3,4,1,7,9,8,6]
+    print(sort(a, 0, len(a) - 1))
