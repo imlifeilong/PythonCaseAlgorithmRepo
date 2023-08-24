2be2fb481738570b89084c5f2c327b09e36f8245
imlifeilong <13772032410@163.com>
2023-08-24 23:30:44 +0800

    8-24

--- basic/排序/排序-快速排序.py
+++ basic/排序/排序-快速排序.py
@@ -24,44 +24,104 @@
 递归到最底部时，数列的大小是零或一，也就是已经排序好了。这个算法一定会结束，因为在每次的迭代（iteration）中，它至少会把一个元素摆到它最后的位置去。
 '''
 
-def swift(alist, low, hight):
-    # 选择第一个数作为基点，位置为t0
-    item = alist[low]
-    while low < hight:
-        #从右向左依次和基点的数比较，位置为t1，如果大于基点，位置向左挪1位(t1=t1-1)
-        while alist[hight] >= item and low < hight:
-            hight -= 1
 
-        #如果有小于基点的数，放到t0，左端位置向右挪1位(t2=t2+1)
-        if low < hight:
-            alist[low] = alist[hight]
-            low += 1
+def swift_1(data, left, right):
+    item = data[left]
+    mid = left
+    index = left
+    while index < right:
+        # 当left所指的值 小于基准值时，移动到mid左边
+        print(data[left], item, mid, index, data)
+        if data[index] < item:
+            data[index], data[mid] = data[mid], data[index]
+            mid += 1
+        index += 1
+    data[left], data[mid] = data[mid], data[left]
+    print(data)
 
-        #从左往右找大于基点的数，位置为t2，如果小于基点，位置向右挪1位(t2=t2+1)
-        while alist[low] < item and low < hight:
-            low += 1
 
-        #如果有大于基点的数，放到t2，右端的位置向左挪1位(t1=t1-1)
-        if low < hight:
-            alist[hight] = alist[low]
-            hight -= 1
+def swift_2(data, left, right):
+    # 单路快排
+    # 二路快排 为了解决让数据更平均的分布再基准值两边
 
-    #调整完后基点左边的数小于基点，右边的数大于基点
-    alist[low] = item
+    # 最左边的选为基准值，拿出来后，此时left的位置是空的
+    item = data[left]
+    # 应该有一个置空的操作
+    # data[left] = None
+    while left < right:
+        # 从右往左扫，直到扫到小于基准值的 停下来，
+        # 扫的时候要满足left < right 比如 [0,1,2,3,4] 这种数据，如果选第一个数为基准值，
+        # 从右往左扫描的时候，直到第一个数也找不到一个数小于基准值的，
+        # 不限制的话 right就会继续向左，从而越界，导致错误
+        while left < right and data[right] >= item:
+            right -= 1
+
+        # 如果left和right未相遇，把找到的那个小值，交换到基准值的位置，
+        # 并将left向右移动，此时 right的位置为空了
+        if left < right:
+            data[left] = data[right]
+            # 应该有一个置空的操作
+            # data[right] = None
+            left += 1
+
+        # 然后从左往右扫，直到扫到大于或等于基准值的 停下来
+        # 同理 [4,3,2,1,0] 的情况，如果第一个数为基准，从左往右扫描的时候，
+        # 直到最后一个数也找不到一个大于等于基准数的值
+        # 如果不限制的话，left就会继续向右，导致越界
+        while left < right and data[left] < item:
+            left += 1
+
+        # 如果left和right未相遇，把找到的那个大值，交换到right的位置，并将right向左移动，此时left的位置为空
+        if left < right:
+            data[right] = data[left]
+            # 应该有一个置空的操作
+            # data[left] = None
+            right -= 1
+
+    # 当left和right相遇的时候，就是要好的mid的位置，即mid左边都是小于基准值，mid右边都是大于等于基准值
+    # 所以将基准值放到mid的位置
+    data[left] = item
     # 返回调整后基点的位置
-    return low
-    
+    return left
+
+
+def swift_3(data, left, right):
+    """
+    三路快排
+    [0, less) 小于 base
+    [less, left) 等于 base
+    [left, more] 未处理，所以要遍历的区域
+    (more, right) 大于base
+    """
+    less = left
+    more = right
+    base = data[right]
+    while left < more:
+        if data[left] < base:
+            data[left], data[less] = data[less], data[left]
+            left += 1
+            less += 1
+        elif data[left] > base:
+            data[left], data[more] = data[more], data[left]
+            more -= 1
+        else:
+            left += 1
+
+
 def quick(alist, left, right):
-    #二分数列
+    # 二分数列
     if left < right:
-        mid = swift(alist, left, right)
-        quick(alist, left, mid-1)
-        quick(alist, mid+1, right)
+        # 索引 [0,1,2,3,4,5,6]
+        # mid=3时，左边的索引范围0-2 右边的索引范围4-6
+        # 因为每次分隔后，mid的位置就排好了，以后都不会变了，所以不参与以后的排序
+        # 左闭右开的原则，所以左边的索引范围是 left, mid-1 右边的索引范围是 mid+1, right
+        mid = swift_1(alist, left, right)
+        quick(alist, left, mid - 1)
+        quick(alist, mid + 1, right)
     return alist
 
 
 if __name__ == "__main__":
-    alist = [23,4,66,23,4,66,43,14,8,32,43,14,8,32]
-    print(quick(alist, 0, len(alist)-1))
-
-    
+    alist = [23, 4, 66, 23, 4, 66, 43, 14, 8, 32, 43, 14, 8, 32]
+    alist = [5, 4, 2, 6, 1, 8, 9]
+    print(quick(alist, 0, len(alist) - 1))
--- utest/test_b.py
+++ utest/test_b.py
@@ -1,109 +1,127 @@
-# def main(nums, target):
-#     length = len(nums)
-#     for i in range(length):
-#         for j in range(i + 1, length):
-#             if nums[i] + nums[j] == target:
-#                 print(i, j)
-#                 return i, j
+# # def main(nums, target):
+# #     length = len(nums)
+# #     for i in range(length):
+# #         for j in range(i + 1, length):
+# #             if nums[i] + nums[j] == target:
+# #                 print(i, j)
+# #                 return i, j
+# #
+# #
+# # nums = [2, 7, 11, 15]
+# # target = 9
+# #
+# # nums = [3, 2, 4]
+# # target = 6
+# #
+# # nums = [3, 3]
+# # target = 6
+# #
+# # main(nums, target)
 #
 #
-# nums = [2, 7, 11, 15]
-# target = 9
+# res = 0
+# target = 0
+# numsSet = set()
 #
-# nums = [3, 2, 4]
-# target = 6
 #
-# nums = [3, 3]
-# target = 6
+# def main():
+#     global res, target, numsSet
 #
-# main(nums, target)
+#     nums = list(map(int, input().split()))
+#     k = int(input())
+#     target = int(input())
+#
+#     nums.sort()
+#     combine(nums, k, [], 0, 0)
+#
+#     print(res)
+#
+#
+# def combine(nums, n, lst, index, total):
+#     global res, target, numsSet
+#
+#     if n == 0:
+#         if total == target:
+#             numsSet.add(tuple(lst))
+#             res += 1
+#     else:
+#         for i in range(index, len(nums)):
+#             if i > index and nums[i] == nums[i - 1]:
+#                 continue
+#             if total + nums[i] > target:
+#                 break
+#             lst.append(nums[i])
+#             combine(nums, n - 1, lst, i + 1, total + nums[i])
+#             lst.pop()
+#
+#
+# # main()
+#
+#
+# def ks(data, k, target):
+#     tmp = []
+#     res = process(0, 0, data, k, target, tmp)
+#     print(res)
+#
+#
+# def process(index, total, data, k, target, tmp):
+#     """
+#     第index个数的和时total
+#     :param index:
+#     :param total:
+#     :param data:
+#     :param k:
+#     :param target:
+#     :return:
+#     """
+#     # # 当k个数的和为target时，方案数+1
+#     if k == 0 and total == target:
+#         tmp.sort()
+#         print(tmp)
+#         return 1
+#
+#     if index >= len(data) or (k <= 0 and total != target):
+#         return 0
+#
+#     cur = data[index]
+#     print(f'当前值 {cur} 还剩{k - 1}')
+#
+#     # 选择当前值
+#     tmp.append(data[index])
+#     p1 = process(index + 1, total + cur, data, k - 1, target, tmp)
+#     # 回溯
+#     tmp.pop()
+#
+#     # 不选当前值
+#     p2 = process(index + 1, total, data, k, target, tmp)
+#
+#     return p1 + p2
+#
+#
+# data = [int(i) for i in '-1 0 1 2 -1 -4'.split()]
+# k = 3
+# target = 0
+#
+# # data = [int(i) for i in '2 7 11 15'.split()]
+# # k = 2
+# # target = 9
+#
+# ks(data, k, target)
+class A:
+    def test(self):
+        print('A')
 
 
-res = 0
-target = 0
-numsSet = set()
+class B:
+    def test(self):
+        print('B')
 
 
-def main():
-    global res, target, numsSet
-
-    nums = list(map(int, input().split()))
-    k = int(input())
-    target = int(input())
-
-    nums.sort()
-    combine(nums, k, [], 0, 0)
-
-    print(res)
+class C(A, B):
+    def __init__(self):
+        super().test()  # 调用A类中的test方法
+        super(C, self).test()  # 调用A类中的test方法
+        super(A, self).test()  # 调用B类中的test方法
 
 
-def combine(nums, n, lst, index, total):
-    global res, target, numsSet
-
-    if n == 0:
-        if total == target:
-            numsSet.add(tuple(lst))
-            res += 1
-    else:
-        for i in range(index, len(nums)):
-            if i > index and nums[i] == nums[i - 1]:
-                continue
-            if total + nums[i] > target:
-                break
-            lst.append(nums[i])
-            combine(nums, n - 1, lst, i + 1, total + nums[i])
-            lst.pop()
-
-
-# main()
-
-
-def ks(data, k, target):
-    tmp = []
-    res = process(0, 0, data, k, target, tmp)
-    print(res)
-
-
-def process(index, total, data, k, target, tmp):
-    """
-    第index个数的和时total
-    :param index:
-    :param total:
-    :param data:
-    :param k:
-    :param target:
-    :return:
-    """
-    # # 当k个数的和为target时，方案数+1
-    if k == 0 and total == target:
-        tmp.sort()
-        print(tmp)
-        return 1
-
-    if index >= len(data) or (k <= 0 and total != target):
-        return 0
-
-    cur = data[index]
-    print(f'当前值 {cur} 还剩{k - 1}')
-
-    # 选择当前值
-    tmp.append(data[index])
-    p1 = process(index + 1, total + cur, data, k - 1, target, tmp)
-    # 回溯
-    tmp.pop()
-
-    # 不选当前值
-    p2 = process(index + 1, total, data, k, target, tmp)
-
-    return p1 + p2
-
-
-data = [int(i) for i in '-1 0 1 2 -1 -4'.split()]
-k = 3
-target = 0
-
-# data = [int(i) for i in '2 7 11 15'.split()]
-# k = 2
-# target = 9
-
-ks(data, k, target)
+C()
