c14f5c6bd41457cde289e1922cf1048e8c078694
lifeilong <13772032410@163.com>
2024-10-09 21:36:21 +0800

    10-09

---
+++ pythonlearn/08_django/django_01_with_route/account/views.py
@@ -0,0 +1,10 @@
+import json
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def login(request):
+    return HttpResponse(json.dumps({"msg": "okok"}, ensure_ascii=False))
--- pythonlearn/08_django/django_01_with_route/django_01_with_route/urls.py
+++ pythonlearn/08_django/django_01_with_route/django_01_with_route/urls.py
@@ -17,7 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+"""
+为什么需要命名空间？
+在Django项目中，URL名称（也称为URL别名或URL标识符）用于在视图中引用URL。
+如果没有命名空间，那么当多个应用或应用的多个部分尝试使用相同的URL名称时，就会发生冲突。
+命名空间允许你通过前缀来区分这些名称，从而避免冲突。可以通过在include()函数中指定namespace参数来设置命名空间。
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('app/', include('app.urls')),
+    path('account/', include('account.urls', namespace="account")),
 ]
