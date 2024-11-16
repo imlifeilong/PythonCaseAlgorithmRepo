b5b0d55da86f6c24766d4a0585fbe877cb563697
lifeilong <13772032410@163.com>
2024-11-16 20:48:38 +0800

    1116

--- pythonlearn/08_django/django_08_with_api/app/admin.py
+++ pythonlearn/08_django/django_08_with_api/app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from app.models import Book
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # 显示列
+    list_display = ('title', 'author', 'create_time')
+    # 搜索
+    search_fields = ('title', 'author')
+    # 过滤
+    list_filter = ('author',)
+
+    date_hierarchy = 'online_time'
+
+    ordering = ('-create_time',)
+
+    fields = ('title', 'author', 'price')
+
+    exclude = ('translator',)
+
+    readonly_fields = ('create_time',)
+
+    def mark_as_sold(self, request, queryset):
+        queryset.update(sold=True)
+
+    mark_as_sold.short_description = "批量标记为已售"
+
+    actions = [mark_as_sold]
+
+
+admin.site.register(Book, BookAdmin)
